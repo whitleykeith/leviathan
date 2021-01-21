60eb85933dc4faf2b93c5f5df94defb8b548af2a
Keith Whitley <kwhitley@redhat.com>
2021-01-21 14:06:33 -0500

    fixing namespace issue

--- install.sh
+++ install.sh
@@ -47,7 +47,7 @@ install_airflow_workaround(){
 }
 
 install_argo(){
-    kubectl create namespace airflow
+    kubectl create namespace argocd
     kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 }
 
